108bf125429f38f876fce41d9f1d03a784a94e8d
Amitabh08 <133643919+Amitabh08@users.noreply.github.com>
2023-10-05 09:44:52 +0530

    Add files via upload

---
+++ Assignment_day10/Assignment1.py
@@ -0,0 +1,38 @@
+from math import sqrt
+
+def check_prime(num):
+    if(num==1):
+        return False
+    for i in range(2,int(sqrt(num))+1):
+        if(num%i==0):
+            return False
+    return True
+
+class PrimeIterator:
+    def __init__(self, start, end=None):
+        if end == None:
+            self.i = 1
+            self.j = start
+        else:
+            self.i = start
+            self.j = end
+
+    def __iter__(self):
+        self.n = self.i -1
+        return self
+
+    def __next__(self):
+        if self.n <= self.j:
+            self.n += 1
+            while not check_prime(self.n):
+                self.n += 1
+                if self.n > self.j:
+                        raise StopIteration
+            else:
+                return self.n
+        else:
+            raise StopIteration
+
+a = iter(PrimeIterator(1000,10000))
+for i in a:
+    print(i, end = " ")
